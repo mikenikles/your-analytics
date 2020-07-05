769b5e498db34f6b548fa86436c96cb9fc899082
Mike Nikles <mikenikles@gmail.com>
2020-07-05 17:09:48 +0100

    Promisify the ClickHouse recordEvent function.

--- services/events-api/src/clickhouse.js
+++ services/events-api/src/clickhouse.js
@@ -8,31 +8,35 @@ const ch = new ClickHouse({
 });
 
 const recordEvent = (event) => {
-  const writableStream = ch.query(`INSERT INTO your-analytics.events FORMAT TSV`, (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log('Event recorded!');
+  return new Promise((resolve, reject) => {
+    const writableStream = ch.query(`INSERT INTO your-analytics.events FORMAT TSV`, (err) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      } else {
+        console.log('Event recorded!');
+        resolve();
+      }
+    });
+
+    // data will be formatted for you
+    writableStream.write([
+      new Date(), // timestamp DateTime
+      event.name, // name String,
+      event.domain, // domain String,
+      event.user_id, // user_id UInt64,
+      event.session_id, // session_id UInt64,
+      event.hostname, // hostname String,
+      event.path, // path String,
+      event.referrer, // referrer String,
+      event.country_code, // country_code LowCardinality(FixedString(2)),
+      event.screen_size, // screen_size LowCardinality(String),
+      event.operating_system, // operating_system LowCardinality(String),
+      event.browser, // browser LowCardinality(String)
+    ]);
+
+    writableStream.end();
   });
-
-  // data will be formatted for you
-  writableStream.write([
-    new Date(), // timestamp DateTime
-    event.name, // name String,
-    event.domain, // domain String,
-    event.user_id, // user_id UInt64,
-    event.session_id, // session_id UInt64,
-    event.hostname, // hostname String,
-    event.path, // path String,
-    event.referrer, // referrer String,
-    event.country_code, // country_code LowCardinality(FixedString(2)),
-    event.screen_size, // screen_size LowCardinality(String),
-    event.operating_system, // operating_system LowCardinality(String),
-    event.browser, // browser LowCardinality(String)
-  ]);
-
-  writableStream.end();
 };
 
 module.exports = {
