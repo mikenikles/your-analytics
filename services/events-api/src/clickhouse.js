6f4b14bef6e14abdc3ca833c302ecfbe73a36041
Mike Nikles <mikenikles@gmail.com>
2020-07-05 20:14:48 +0100

    Clean up code.

--- services/events-api/src/clickhouse.js
+++ services/events-api/src/clickhouse.js
@@ -7,39 +7,18 @@ const ch = new ClickHouse({
   password: process.env.CH_PASSWORD
 });
 
-const recordEvent = (event) => {
-  console.log("Recording event: ", JSON.stringify(event));
-
-  return new Promise((resolve, reject) => {
-    const writableStream = ch.query(`INSERT INTO youranalytics.events`, { format: 'JSONEachRow' }, (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        console.log('Event recorded!');
-        resolve();
-      }
-    });
-
-    // data will be formatted for you
-    writableStream.write({...event, timestamp: new Date()});
-    // writableStream.write([
-    //   new Date(), // timestamp DateTime
-    //   event.name, // name String,
-    //   event.domain, // domain String,
-    //   event.user_id, // user_id UInt64,
-    //   event.session_id, // session_id UInt64,
-    //   event.hostname, // hostname String,
-    //   event.path, // path String,
-    //   event.referrer, // referrer String,
-    //   event.country_code, // country_code LowCardinality(FixedString(2)),
-    //   event.screen_size, // screen_size LowCardinality(String),
-    //   event.operating_system, // operating_system LowCardinality(String),
-    //   event.browser, // browser LowCardinality(String)
-    // ]);
-
-    writableStream.end();
+const recordEvent = (event) => new Promise((resolve, reject) => {
+  const writableStream = ch.query(`INSERT INTO youranalytics.events`, { format: 'JSONEachRow' }, (err) => {
+    if (err) {
+      reject(err);
+    } else {
+      console.log('Event recorded!');
+      resolve();
+    }
   });
-};
+  writableStream.write({ ...event, timestamp: new Date() });
+  writableStream.end();
+});
 
 module.exports = {
   recordEvent
