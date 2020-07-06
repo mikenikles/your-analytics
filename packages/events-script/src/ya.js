972f0a4e23166412e68f2ebeb2b7ab5966cbe278
Mike Nikles <mikenikles@gmail.com>
2020-07-06 20:30:52 +0000

    Clean up the ya.js script; send minimum parameters only.

--- packages/events-script/src/ya.js
+++ packages/events-script/src/ya.js
@@ -1,39 +1,37 @@
 (() => {
-  const sendEvent = () => {
-    fetch("https://events-api-sizrfb2hwq-uc.a.run.app", {
-      method: 'POST',
-      body: JSON.stringify({
-        name: "pageview",
-        domain: "TODO",
-        user_id: 1,
-        session_id: 9,
-        hostname: location.hostname,
-        path: location.pathname,
-        referrer: document.referrer,
-        // country_code: "",
-        screen_size: `${window.innerWidth}x${window.innerHeight}`,
-        // operating_system: "",
-        // browser: ""
-      }),
-      credentials: 'omit',
-      cache: 'no-store',
-      mode: 'no-cors',
-      headers: {
-        'Content-Type': 'text/plain'
-      }
-    });
-  };
-
   try {
-    const h = window.history
-    // if (h.pushState) {
-      // const originalPushState = h['pushState']
-      // h.pushState = function () {
-      //   originalPushState.apply(this, arguments);
-      //   sendEvent();
-      // };
-      window.addEventListener('popstate', sendEvent);
-    // }
+    const sendEvent = () => {
+      fetch("https://events-api-sizrfb2hwq-uc.a.run.app", {
+        method: 'POST',
+        body: JSON.stringify({
+          name: "pageview",
+          domain: document.querySelector('[src*="your-analytics.vercel"]').getAttribute("data-domain"),
+          url: location.protocol + '//' + location.hostname + location.pathname + location.search,
+          // user_id: 1,
+          // session_id: 9,
+          referrer: document.referrer,
+          screen_size: `${window.innerWidth}x${window.innerHeight}`
+        }),
+        credentials: 'omit',
+        cache: 'no-store',
+        mode: 'no-cors',
+        headers: {
+          'Content-Type': 'text/plain'
+        }
+      });
+    };
+
+    const originalHistory = window.history;
+    if (originalHistory.pushState) {
+      const originalPushState = originalHistory['pushState'];
+      originalHistory.pushState = function () {
+        originalPushState.apply(this, arguments);
+        sendEvent();
+      };
+      window.addEventListener('popstate', () => {
+        sendEvent();
+      });
+    }
     sendEvent();
   } catch (error) {
     // TODO: Capture and send to events-api for troubleshooting
