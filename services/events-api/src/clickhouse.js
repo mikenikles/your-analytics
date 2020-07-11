3809650ec56d7f4b32d74d509c5a51c9ea8cac9a
Mike Nikles <mikenikles@gmail.com>
2020-07-11 12:53:21 +0100

    Prepare the events-api service for local development.

--- services/events-api/src/clickhouse.js
+++ services/events-api/src/clickhouse.js
@@ -8,19 +8,14 @@ const ch = new ClickHouse({
 });
 
 const recordEvent = (event) => new Promise((resolve, reject) => {
-  console.log("Recording event:", JSON.stringify(event));
+  // console.log("Recording event:", JSON.stringify(event));
   const writableStream = ch.query(`INSERT INTO youranalytics.events`, {
     format: 'JSONEachRow',
     queryOptions: {
       input_format_import_nested_json: 1
     }
   }, (err) => {
-    if (err) {
-      reject(err);
-    } else {
-      console.log('Event recorded!');
-      resolve();
-    }
+    err ? reject(err) : resolve();
   });
   writableStream.write({ ...event, timestamp: new Date() });
   writableStream.end();
