784b6441b611981f8540e96c8e1bf50d72c507c9
Mike Nikles <mikenikles@gmail.com>
2020-07-11 13:04:03 +0000

    Remove Sapper boilerplate; add index title and meta description.

--- services/website/src/routes/index.svelte
+++ services/website/src/routes/index.svelte
@@ -1,46 +1,4 @@
-<style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
-
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Collect events from websites, native apps and IoT devices | Your Analytics</title>
+  <meta name="description" content="An analytis platform to collect events, such as from website visits, user behavior in native apps or IoT-enabled devices.">
 </svelte:head>
-
-<h1>Great success!</h1>
-
-<figure>
-	<img alt='Success Kid' src='successkid.jpg'>
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
