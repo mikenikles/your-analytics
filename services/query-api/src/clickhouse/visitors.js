de91cdea5516c26a7b4ecd791c4bc6aa0d3ef5c2
Mike Nikles <mikenikles@gmail.com>
2020-07-11 09:20:58 +0000

    Add a /visitors endpoint that returns dummy data.

---
+++ services/query-api/src/clickhouse/visitors.js
@@ -0,0 +1,13 @@
+const fetchVisitors = () => {
+  return {
+    visitors: [{
+      "January": 128,
+      "February": 211,
+      "March": 167,
+    }]
+  }
+};
+
+module.exports = {
+  fetchVisitors
+};
