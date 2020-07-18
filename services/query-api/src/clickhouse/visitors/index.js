a9a6c60a37683c197216883759ad30324f4707eb
Mike Nikles <mikenikles@gmail.com>
2020-07-18 20:32:57 +0000

    Use dev data in development rather than calling ClickHouse.

--- services/query-api/src/clickhouse/visitors/index.js
+++ services/query-api/src/clickhouse/visitors/index.js
@@ -1,4 +1,11 @@
+const devData = require("./dev-data.json");
+
+const IS_DEV = process.env.NODE_ENV === "development";
+
+const fetchVisitorsDev = () => () => devData;
+
 const fetchVisitors = (ch) => async () => {
+  console.log(process.env.NODE_ENV);
   const sql = `SELECT toDate(timestamp) AS day, COUNT(*) AS total FROM youranalytics.events GROUP BY day`;
   const stream = ch.query(sql);
 
@@ -18,5 +25,5 @@ const fetchVisitors = (ch) => async () => {
 };
 
 module.exports = {
-  fetchVisitors,
+  fetchVisitors: IS_DEV ? fetchVisitorsDev : fetchVisitors,
 };
