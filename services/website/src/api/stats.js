3e281895bbb6839908706ccb76c58c7a8bedbe9e
Mike Nikles <mikenikles@gmail.com>
2020-07-18 22:00:29 +0000

    Fetch & display top pages.

--- services/website/src/api/stats.js
+++ services/website/src/api/stats.js
@@ -2,8 +2,8 @@ import { get, writable } from "svelte/store";
 import { QUERY_API_BASE_URL } from "../config";
 import { userTokenStore } from "../auth/magic";
 
-const fetchStats = async (type, store) => {
-  const response = await fetch(`${QUERY_API_BASE_URL}/${type}`, {
+const fetchStats = async (path, store) => {
+  const response = await fetch(`${QUERY_API_BASE_URL}/${path}`, {
     headers: new Headers({
       Authorization: "Bearer " + get(userTokenStore),
     }),
@@ -13,6 +13,9 @@ const fetchStats = async (type, store) => {
     store.set((await response.json()).data);
   }
 };
+
+export const topPages = writable(null);
+export const fetchTopPages = () => fetchStats("top-pages", topPages);
 
 export const visitors = writable(null);
 export const fetchVisitors = () => fetchStats("visitors", visitors);
---
+++ services/website/src/components/stats/top-pages.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { topPages } from "../../api/stats";
+</script>
+
+<h2>Top Pages</h2>
+
+{#each Object.entries($topPages) as [page, total]}
+  <div>
+    <p>{page}: {total}</p>
+  </div>
+{/each}
