b0612811101fd59049a12b7d6879045c29af28c6
Mike Nikles <mikenikles@gmail.com>
2020-07-19 12:26:09 +0000

    Auto-determine the environment to provide the correct config values in the website service.

--- services/website/src/config.js
+++ services/website/src/config.js
@@ -1,10 +1,32 @@
-export const ADMIN_API_BASE_URL =
-  "https://8082-fe64c5f9-d995-4313-b604-3351faf8883a.ws-eu01.gitpod.io";
-// export const ADMIN_API_BASE_URL = "https://admin-api.your-analytics.org";
+const config = {
+  development: {
+    adminApiBaseUrl:
+      "https://8082-fa85e924-90d4-429b-a4b4-fa3bb51b0a3e.ws-eu01.gitpod.io",
+    queryApiBaseUrl:
+      "https://8081-fa85e924-90d4-429b-a4b4-fa3bb51b0a3e.ws-eu01.gitpod.io",
+    magicPublicKey: "pk_test_517AC93805DD89CB",
+  },
+  production: {
+    adminApiBaseUrl: "https://admin-api.your-analytics.org",
+    queryApiBaseUrl: "https://query-api.your-analytics.org",
+    magicPublicKey: "pk_live_BA455263710CC21F",
+  },
+};
 
-export const QUERY_API_BASE_URL =
-  "https://8081-fe64c5f9-d995-4313-b604-3351faf8883a.ws-eu01.gitpod.io";
-// export const QUERY_API_BASE_URL = "https://query-api.your-analytics.org";
+const isDevelopment = process.browser
+  ? window.location.hostname.endsWith(".gitpod.io") ||
+    window.location.hostname.endsWith(".vercel.app")
+  : process.env.NODE_ENV === "development";
+const isProduction = process.browser
+  ? window.location.hostname.endsWith(".your-analytics.org")
+  : process.env.NODE_ENV === "production";
 
-export const MAGIC_PUBLIC_KEY = "pk_test_517AC93805DD89CB";
-// export const MAGIC_PUBLIC_KEY = "pk_live_BA455263710CC21F";
+const getEnvironment = () =>
+  isDevelopment ? "development" : isProduction ? "production" : "";
+const getConfigValue = (key) => config[getEnvironment()][key];
+
+export const ADMIN_API_BASE_URL = getConfigValue("adminApiBaseUrl");
+
+export const QUERY_API_BASE_URL = getConfigValue("queryApiBaseUrl");
+
+export const MAGIC_PUBLIC_KEY = getConfigValue("magicPublicKey");
