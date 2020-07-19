1635101f31cc04a7a9a4da2aa471478ea61b03ca
Mike Nikles <mikenikles@gmail.com>
2020-07-19 19:05:52 +0000

    Remove old route handlers.

--- services/query-api/src/index.js
+++ services/query-api/src/index.js
@@ -60,58 +60,6 @@ createStatsEndpoint("/top-referrers", fetchTopReferrers);
 createStatsEndpoint("/visitors", fetchVisitors);
 createStatsEndpoint("/world-map", fetchWorldMap);
 
-// app.get("/top-pages", isAuthenticated, async (req, res) => {
-//   try {
-//     // TODO
-//     // - Load database query parameters based on who called this endpoint
-
-//     const data = await fetchTopPages();
-//     res.json({ data });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).end();
-//   }
-// });
-
-// app.get("/top-referrers", isAuthenticated, async (req, res) => {
-//   try {
-//     // TODO
-//     // - Load database query parameters based on who called this endpoint
-
-//     const data = await fetchTopReferrers();
-//     res.json({ data });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).end();
-//   }
-// });
-
-// app.get("/visitors", isAuthenticated, async (req, res) => {
-//   try {
-//     // TODO
-//     // - Load database query parameters based on who called this endpoint
-
-//     const data = await fetchVisitors();
-//     res.json({ data });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).end();
-//   }
-// });
-
-// app.get("/world-map", isAuthenticated, async (req, res) => {
-//   try {
-//     // TODO
-//     // - Load database query parameters based on who called this endpoint
-
-//     const data = await fetchWorldMap();
-//     res.json({ data });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).end();
-//   }
-// });
-
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`);
 });
