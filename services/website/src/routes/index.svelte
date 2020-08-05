b1703244e3d6b9410a8b83a04cc0827da2abfd49
Mike Nikles <mikenikles@gmail.com>
2020-08-05 19:07:26 +0000

    Add ya.js to the website's index page.

--- services/website/src/routes/index.svelte
+++ services/website/src/routes/index.svelte
@@ -1,4 +1,5 @@
 <svelte:head>
 	<title>Collect events from websites, native apps and IoT devices | Your Analytics</title>
   <meta name="description" content="An analytis platform to collect events, such as from website visits, user behavior in native apps or IoT-enabled devices.">
+  {@html '<script async src="https://your-analytics.org/ya.js" data-domain="your-analytics.org"></script>'}
 </svelte:head>
