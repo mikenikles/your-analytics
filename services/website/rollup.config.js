80f4a7d983cac4ee7a3e8809cea9055364766df0
Mike Nikles <mikenikles@gmail.com>
2020-08-05 18:07:45 +0000

    Remove debug log.

--- services/website/rollup.config.js
+++ services/website/rollup.config.js
@@ -24,8 +24,6 @@ const sveltePreprocessOptions = sveltePreprocess({
   },
 });
 
-console.log("ROLLUP ROLLUP", process.env.VERCEL_GITHUB_COMMIT_SHA);
-
 export default {
   client: {
     input: config.client.input(),
