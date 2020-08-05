9f4ddde96b60f9d9af4d2e748506b8203fd21355
Mike Nikles <mikenikles@gmail.com>
2020-08-05 18:47:54 +0000

    Fix spelling mistake; add parameter to the correct function.

--- services/query-api/src/clickhouse/unique-visitors/index.js
+++ services/query-api/src/clickhouse/unique-visitors/index.js
@@ -2,7 +2,7 @@ const IS_DEV = process.env.NODE_ENV === "development";
 
 const fetchUniqueVisitorsDev = () => () => 14;
 
-const fetchUniqueVisitors = (ch) => async (dateRang, domain) => {
+const fetchUniqueVisitors = (ch) => async (dateRange, domain) => {
   const timezone = "Europe/London";
   const sql = `SELECT COUNT(DISTINCT user_id) AS total FROM youranalytics.events WHERE toUnixTimestamp(timestamp, '${timezone}') >= ${dateRange.from} AND toUnixTimestamp(timestamp, '${timezone}') <= ${dateRange.to} AND domain = '${domain}'`;
 
--- services/query-api/src/clickhouse/visitors/index.js
+++ services/query-api/src/clickhouse/visitors/index.js
@@ -8,10 +8,10 @@ const fetchVisitorsDev = () => () => devData;
  * When a single day is requested, the API expects the "from" date range to be 00:00:00
  * and the "to" date range to be 23:59:59.
  */
-const isDateRangeOneDay = (dateRange, domain) =>
+const isDateRangeOneDay = (dateRange) =>
   dateRange.to - dateRange.from + 1 === 86400;
 
-const fetchVisitors = (ch) => async (dateRange) => {
+const fetchVisitors = (ch) => async (dateRange, domain) => {
   const timezone = "Europe/London";
   const granularity = isDateRangeOneDay(dateRange) ? "Hour" : "Date";
   const sql = `SELECT to${granularity}(timestamp, '${timezone}') AS day, COUNT(DISTINCT user_id) AS total FROM youranalytics.events WHERE toUnixTimestamp(timestamp, '${timezone}') >= ${dateRange.from} AND toUnixTimestamp(timestamp, '${timezone}') <= ${dateRange.to} AND domain = '${domain}' GROUP BY day`;
