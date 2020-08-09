98c4f147965833ad8489abeee081af30363d8cc5
Mike Nikles <mikenikles@gmail.com>
2020-08-09 21:27:20 +0000

    Use an animated loading spinner when needed.

--- services/website/src/components/stats/loading.svelte
+++ services/website/src/components/stats/loading.svelte
@@ -1,12 +1,7 @@
-<div class="border border-gray-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
-  <div class="animate-pulse flex space-x-4">
-    <div class="rounded-full bg-gray-400 h-12 w-12"></div>
-    <div class="flex-1 space-y-4 py-1">
-      <div class="h-4 bg-gray-400 rounded w-3/4"></div>
-      <div class="space-y-2">
-        <div class="h-4 bg-gray-400 rounded"></div>
-        <div class="h-4 bg-gray-400 rounded w-5/6"></div>
-      </div>
-    </div>
-  </div>
-</div>
+<span class="inline-flex items-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-bg-gray-800 focus:outline-none transition ease-in-out duration-150 cursor-wait">
+  <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-bg-gray-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+  </svg>
+  Preparing visualization...
+</span>
