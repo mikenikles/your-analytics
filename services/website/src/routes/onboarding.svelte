4a6b217999e3271a837ab8da98fea71d97da409e
Mike Nikles <mikenikles@gmail.com>
2020-08-14 16:05:25 +0000

    Apply error handling for an invalid website URL.

--- services/website/src/routes/onboarding.svelte
+++ services/website/src/routes/onboarding.svelte
@@ -8,7 +8,10 @@
 
   let url = "";
   let isSiteAdded = false;
+
   $: snippet = `<script async src="https://your-analytics.org/ya.js" data-domain="${url}"><\/script>`;
+  $: existingSites = ($userMetadataStore && Object.keys($userMetadataStore.sites || {})) || [];
+  $: isUrlInvalid = existingSites.includes(url)
 
   const handleSubmit = async (event) => {
     const firstName = new FormData(event.target).get("firstname");
@@ -17,6 +20,12 @@
     isSiteAdded = await addNewWebsite({firstName, url, timezone});
   };
 </script>
+
+<style>
+  button:disabled {
+    @apply bg-indigo-500 cursor-not-allowed;
+  }
+</style>
 
 <Authenticated>
   <Header title="Onboarding" />
@@ -46,12 +55,22 @@
               <label for="url" class="block text-sm font-medium leading-5 text-gray-700">
                 What's your website URL?
               </label>
-              <div class="mt-1 flex rounded-md shadow-sm">
+              <div class="mt-1 relative flex rounded-md shadow-sm">
                 <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 sm:text-sm">
                   http(s)://www.
                 </span>
                 <input id="url" bind:value={url} type="text" name="url" required class="appearance-none flex-1 block w-full px-3 py-2 border border-gray-300 rounded-none rounded-r-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5">
+                {#if isUrlInvalid}
+                  <div class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+                    <svg class="h-5 w-5 text-red-500" fill="currentColor" viewBox="0 0 20 20">
+                      <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
+                    </svg>
+                  </div>
+                {/if}
               </div>
+              {#if isUrlInvalid}
+                <p class="mt-2 text-sm text-red-600" id="url-error">You already configured this website</p>
+              {/if}
             </div>
 
             <div class="mt-6">
@@ -65,7 +84,7 @@
 
             <div class="mt-6">
               <span class="block w-full rounded-md shadow-sm">
-                <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
+                <button type="submit" disabled={isUrlInvalid} class="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
                   Let's go
                 </button>
               </span>
