dd60768f1c1d235637805aa4ee1ac75792639a9c
Mike Nikles <mikenikles@gmail.com>
2020-08-14 20:11:03 +0000

    Convert DB string results to number.

--- services/query-api/src/clickhouse/screen/index.js
+++ services/query-api/src/clickhouse/screen/index.js
@@ -21,13 +21,13 @@ const fetchScreen = (ch) => async (dateRange, domain, timezone) => {
 
       // TODO: Can this be done in SQL?
       if (row[0] <= 640) {
-        result.mobile += row[1];
+        result.mobile += row[1] * 1;
       } else if (row[0] <= 768) {
-        result.tablet += row[1];
+        result.tablet += row[1] * 1;
       } else if (row[0] <= 1024) {
-        result.laptop += row[1];
+        result.laptop += row[1] * 1;
       } else {
-        result.desktop += row[1];
+        result.desktop += row[1] * 1;
       }
     });
 
