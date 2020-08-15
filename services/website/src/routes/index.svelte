e1640fc7530f8e03b1acdb22535819668ed361f2
Mike Nikles <mikenikles@gmail.com>
2020-08-15 14:51:00 +0000

    Re-enable YA analytics script.

--- services/website/src/routes/index.svelte
+++ services/website/src/routes/index.svelte
@@ -14,7 +14,7 @@
 <svelte:head>
 	<title>Web analytics platform. Open source, privacy-focused and simple. | Your Analytics</title>
   <meta name="description" content="An open source web analytis platform with a focus on privacy and simplicity.">
-  <!-- {@html '<script async src="https://your-analytics.org/ya.js" data-domain="your-analytics.org"></script>'} -->
+  {@html '<script async src="https://your-analytics.org/ya.js" data-domain="your-analytics.org"></script>'}
 </svelte:head>
 
 <div class="bg-gray-50">
