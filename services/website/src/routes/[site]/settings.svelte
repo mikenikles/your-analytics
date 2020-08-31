2ce8427a15919ed3a7a97e721f77b7408471e88e
Mike Nikles <mikenikles@gmail.com>
2020-08-31 16:03:30 +0000

    Add the website settings page placeholder.

---
+++ services/website/src/routes/[site]/settings.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { stores } from "@sapper/app";
+  import Authenticated from "../../components/authenticated.svelte";
+  import Header from "../../components/header.svelte";
+  import MainContent from "../../components/main-content.svelte";
+
+  const { page } = stores();
+
+  $: pageTitle = `Settings - ${$page.params.site}`;
+</script>
+
+<Authenticated>
+  <Header title={pageTitle} />
+  <MainContent>
+    Settings go here
+  </MainContent>
+</Authenticated>
