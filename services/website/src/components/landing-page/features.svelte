195a56a148113dd38f708828bbf7ab56f4a1fb93
Mike Nikles <mikenikles@gmail.com>
2020-09-08 19:18:43 +0000

    Update the features intro text.

--- services/website/src/components/landing-page/features.svelte
+++ services/website/src/components/landing-page/features.svelte
@@ -5,7 +5,7 @@
 <div class="py-10 max-w-6xl mx-auto sm:py-32">
   <div class="px-4 sm:px-12">
     <h2 class="text-3xl text-center font-extrabold tracking-wide sm:text-5xl">Your data, your way</h2>
-    <p class="mt-8 text-xl sm:text-2xl">We keep it simple and focus on web analytics metrics that matter by also respecting your visitor's privacy.</p>
+    <p class="mt-8 text-xl sm:text-2xl">See your analytics on a web dashboard, export it to a spreadsheet or process events in real-time within your own environment.</p>
   </div>
   <div class="pt-10 flex flex-col sm:flex-row">
     <FeatureCard title="Dashboard" icon="template" description={`Use the web-based dashboard to see your website analytics at a glance. Slice and dice the data with filters available for all metrics, e.g. date range, referrer, page, country, etc.`} />
