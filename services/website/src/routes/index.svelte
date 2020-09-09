9e2f79deb920bf7faa1663647e51ca162af45581
Mike Nikles <mikenikles@gmail.com>
2020-09-09 19:25:15 +0000

    Remove unused code.

--- services/website/src/routes/index.svelte
+++ services/website/src/routes/index.svelte
@@ -1,41 +1,10 @@
 <script>
-  import { sub } from "date-fns";
-  import { onMount } from "svelte";
-  import { recordEmail } from "../api/beta";
-  import { dateRange, siteVisibility } from "../api/stats";
-  import Card from "../components/card.svelte";
   import Features from "../components/landing-page/features.svelte";
   import Header from "../components/header/index.svelte";
   import Hero from "../components/landing-page/hero.svelte";
   import Stats from "../components/landing-page/stats.svelte";
   import FrontendIntegrations from "../components/landing-page/frontend-integrations.svelte";
   import Pricing from "../components/landing-page/pricing.svelte";
-
-  import Devices from "../components/stats/devices.svelte";
-  import TopPages from "../components/stats/top-pages.svelte";
-  import TopReferrers from "../components/stats/top-referrers.svelte";
-  import TotalPageviews from "../components/stats/total-pageviews.svelte";
-  import UniqueVisitors from "../components/stats/unique-visitors.svelte";
-  import Visitors from "../components/stats/visitors.svelte";
-  import WorldMap from "../components/stats/world-map.svelte";
-
-  let isMobileMenuOpen = false;
-  let isEmailAdded;
-
-  const handleSubmit = async (event) => {
-    const email = new FormData(event.target).get("email");
-    isEmailAdded = await recordEmail(email);
-  };
-
-  onMount(() => {
-    siteVisibility.set("public");
-    dateRange.set({
-      from: sub(new Date(), {
-        days: 30
-      }).getTime(),
-      to: new Date().getTime()
-    })
-  });
 </script>
 
 <svelte:head>
