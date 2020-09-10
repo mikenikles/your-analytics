896fd8517e367cb45877181edea4309cc7328909
Mike Nikles <mikenikles@gmail.com>
2020-09-10 18:24:51 +0000

    Create a Table component.

---
+++ services/website/src/components/table/row.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let rowIndex;
+</script>
+
+<style>
+  .even {
+    @apply bg-gray-100;
+  }
+
+  .odd {
+    @apply bg-white;
+  }
+
+  tr :global(td:first-child) {
+    @apply rounded-l-full;
+  }
+
+  tr :global(td:last-child) {
+    @apply rounded-r-full;
+  }
+</style>
+
+<tr class:even={rowIndex % 2 === 0} class:odd={rowIndex % 2 !== 0}>
+  <slot />
+</tr>
