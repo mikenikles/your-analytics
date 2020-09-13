8e14d7739000c299816c3c0429d2863b929e173d
Mike Nikles <mikenikles@gmail.com>
2020-09-13 22:42:48 +0000

    Proxy locally only; let Vercel take care of it in preview and prod environments.

--- services/website/src/server.js
+++ services/website/src/server.js
@@ -17,14 +17,16 @@ const devProxyOptions = {
 const app = polka();
 app
   .use(
-    createProxyMiddleware("/api/admin", {
-      ...devProxyOptions,
-      target: "http://localhost:8082",
-    }),
-    createProxyMiddleware("/api/query", {
-      ...devProxyOptions,
-      target: "http://localhost:8081",
-    }),
+    dev &&
+      createProxyMiddleware("/api/admin", {
+        ...devProxyOptions,
+        target: "http://localhost:8082",
+      }),
+    dev &&
+      createProxyMiddleware("/api/query", {
+        ...devProxyOptions,
+        target: "http://localhost:8081",
+      }),
     compression({ threshold: 0 }),
     sirv("static", { dev }),
     cookieParser(COOKIE_SECRET),
