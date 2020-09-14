aadd73be01a82412e6f4508ed8e5bc014c249c2a
Mike Nikles <mikenikles@gmail.com>
2020-09-14 08:48:13 +0000

    Remove unused code on the dashboard.

--- services/website/src/routes/[site]/index.svelte
+++ services/website/src/routes/[site]/index.svelte
@@ -13,7 +13,6 @@
 </script>
 
 <script>
-  import { stores } from "@sapper/app";
   import { fetchAllStats } from "../../api/stats";
   import Card from "../../components/card.svelte";
   import DateRange from "../../components/date-range.svelte";
@@ -28,64 +27,46 @@
   import Visitors from "../../components/stats/visitors.svelte";
   import WorldMap from "../../components/stats/world-map.svelte";
 
-  const { page } = stores();
-
-  let isReadyToDisplayStats = true;
-
-  $: pageTitle = `Dashboard - ${$page.params.site}`;
-
   fetchAllStats();
 </script>
 
 <div>
-  <Header title={pageTitle} />
+  <Header />
   <MainContent>
-    {#if isReadyToDisplayStats}
-      <WebsiteSelect />
-      <DateRange />
-      <Card>
-        <div class="flex">
-          <UniqueVisitors />
-          <TotalPageviews />
-        </div>
-      </Card>
-      <Card>
-        <Visitors />
-      </Card>
-      <div class="md:flex md:justify-between">
-        <div class="md:w-1/2">
-          <Card>
-            <TopPages />
-          </Card>
-        </div>
-        <div class="md:w-1/2 md:ml-5">
-          <Card>
-            <TopReferrers />
-          </Card>
-        </div>
+    <WebsiteSelect />
+    <DateRange />
+    <Card>
+      <div class="flex">
+        <UniqueVisitors />
+        <TotalPageviews />
       </div>
-      <div class="md:flex md:justify-between">
-        <div class="md:w-1/2">
-          <Card>
-            <Devices />
-          </Card>
-        </div>
-        <div class="md:w-1/2 md:ml-5">
-          <Card>
-            <WorldMap />
-          </Card>
-        </div>
+    </Card>
+    <Card>
+      <Visitors />
+    </Card>
+    <div class="md:flex md:justify-between">
+      <div class="md:w-1/2">
+        <Card>
+          <TopPages />
+        </Card>
       </div>
-    {:else}
-      <p>
-        <span class="inline-flex items-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-bg-gray-800 focus:outline-none transition ease-in-out duration-150 cursor-wait">
-          <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-bg-gray-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-          </svg>
-          Crunching the numbers for your analytics...
-        </span>
-      </p>
-    {/if}
+      <div class="md:w-1/2 md:ml-5">
+        <Card>
+          <TopReferrers />
+        </Card>
+      </div>
+    </div>
+    <div class="md:flex md:justify-between">
+      <div class="md:w-1/2">
+        <Card>
+          <Devices />
+        </Card>
+      </div>
+      <div class="md:w-1/2 md:ml-5">
+        <Card>
+          <WorldMap />
+        </Card>
+      </div>
+    </div>
   </MainContent>
 </div>
