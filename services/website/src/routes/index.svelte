80c3ed94f51a2359d142372190e1c074eccf1cbb
Mike Nikles <mikenikles@gmail.com>
2020-09-15 14:19:51 +0000

    Initialize the filters on the landing page.

--- services/website/src/routes/index.svelte
+++ services/website/src/routes/index.svelte
@@ -1,7 +1,9 @@
 <script context="module">
   import { fetchAllStats } from "../api/stats";
+  import { initFilters } from "../stores/stats-filters-query";
 
   export async function preload(page) {
+    initFilters(page.query);
     const statsResults = await fetchAllStats(this.fetch, page.host, "your-analytics.org");
     const stats = {};
 
