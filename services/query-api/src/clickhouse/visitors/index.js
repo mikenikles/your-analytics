e1d529f18df36e4d57bf905bb349e2235670afcb
Mike Nikles <mikenikles@gmail.com>
2020-09-17 22:29:18 +0000

    Auto-group the visitors query based on the provided date range.

--- services/query-api/src/clickhouse/visitors/index.js
+++ services/query-api/src/clickhouse/visitors/index.js
@@ -1,18 +1,48 @@
 const devData = require("./dev-data.json");
 
 const IS_DEV = process.env.NODE_ENV === "development";
+const ONE_DAY = 60 * 60 * 24;
+const ONE_MONTH = ONE_DAY * 31;
+const ONE_YEAR = ONE_MONTH * 12;
 
 const fetchVisitorsDev = () => () => devData;
 
 /**
- * When a single day is requested, the API expects the "from" date range to be 00:00:00
- * and the "to" date range to be 23:59:59.
+ * Each `dateRange.to` value represents 23:59:59 on a given day.
+ * The `+ 1` in the calculations below are there to represent a full day.
  */
+const dateRangeGranularities = [
+  {
+    test: ({ from, to }) => to - from + 1 === ONE_DAY,
+    granularity: "Hour",
+  },
+  {
+    test: ({ from, to }) =>
+      to - from + 1 > ONE_DAY && to - from + 1 <= ONE_MONTH,
+    granularity: "Date",
+  },
+  {
+    test: ({ from, to }) =>
+      to - from + 1 > ONE_MONTH && to - from + 1 <= ONE_YEAR,
+    granularity: "Month",
+  },
+  {
+    test: ({ from, to }) => to - from + 1 > ONE_YEAR,
+    granularity: "Year",
+  },
+  {
+    test: () => true,
+    granularity: "Date",
+  },
+];
+
 const isDateRangeOneDay = (dateRange) =>
   dateRange.to - dateRange.from + 1 === 86400;
 
 const fetchVisitors = (ch) => async (dateRange, domain, timezone) => {
-  const granularity = isDateRangeOneDay(dateRange) ? "Hour" : "Date";
+  const { granularity } = dateRangeGranularities.find((dateRangeGranularity) =>
+    dateRangeGranularity.test(dateRange)
+  );
   const sql = `SELECT to${granularity}(timestamp, '${timezone}') AS day, COUNT(DISTINCT user_id) AS total FROM youranalytics.events WHERE toUnixTimestamp(timestamp, '${timezone}') >= ${dateRange.from} AND toUnixTimestamp(timestamp, '${timezone}') <= ${dateRange.to} AND domain = '${domain}' GROUP BY day`;
 
   const stream = ch.query(sql);
