b4c4276ae2223b423f623517805c205fbacc22cf
Mike Nikles <mikenikles@gmail.com>
2020-09-19 16:40:46 +0000

    Format the Number component code.

--- services/website/src/components/stats/elements/number.svelte
+++ services/website/src/components/stats/elements/number.svelte
@@ -1,17 +1,17 @@
 <script>
-	export let number;
-	export let locale = "en";
+  export let number;
+  export let locale = "en";
 
-	$: formattedNumber = number.toLocaleString(locale, {
-		notation: "compact",
-		compactDisplay: "short"
-	});
+  $: formattedNumber = number.toLocaleString(locale, {
+    notation: "compact",
+    compactDisplay: "short",
+  });
 </script>
 
 <style>
-	span {
-		font-variant-numeric: tabular-nums;
-	}
+  span {
+    font-variant-numeric: tabular-nums;
+  }
 </style>
 
 <span class="tabular-nums">{formattedNumber}</span>
