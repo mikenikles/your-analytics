56595142245e16dc79fc37b732778f0d47d24e9e
Mike Nikles <mikenikles@gmail.com>
2020-09-20 15:39:40 +0000

    Remove the error and enable the Let's Go button when the URL input field gains focus.

--- services/website/src/routes/onboarding.svelte
+++ services/website/src/routes/onboarding.svelte
@@ -89,7 +89,7 @@
                   <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 sm:text-sm">
                     http(s)://www.
                   </span>
-                  <input id="url" bind:value={url} type="text" name="url" required class="px-3 py-2 w-full border border-gray-300 rounded-none rounded-r-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5">
+                  <input id="url" bind:value={url} on:focus={() => isSiteAlreadyConfiguredGlobally = false} type="text" name="url" required class="px-3 py-2 w-full border border-gray-300 rounded-none rounded-r-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5">
                   {#if isUrlInvalid}
                     <div class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
                       <svg class="h-5 w-5 text-red-500" fill="currentColor" viewBox="0 0 20 20">
