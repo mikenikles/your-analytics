70392346b5804ab733965ae4a16ba7ff53033848
Mike Nikles <mikenikles@gmail.com>
2020-09-28 23:19:00 +0000

    Display a nav and mobile menu when the user is authenticated.

--- services/website/src/components/header/index.svelte
+++ services/website/src/components/header/index.svelte
@@ -1,26 +1,39 @@
 <script>
-  import { stores } from "@sapper/app";
+  import { goto, stores } from "@sapper/app";
   import { logout } from "../../auth/magic";
   import NavMobile from "./nav-mobile.svelte";
   import NavMobileMenuButton from "./nav-mobile-menu-button.svelte";
   import Nav from "./nav.svelte";
   import NavItem from "./nav-item.svelte";
 
-  const { page } = stores();
+  const { page, session } = stores();
+
+  $: user = $session.user || {};
+  $: userWebsites = Object.keys(user.sites || {});
+  $: hasMultipleSites = userWebsites.length > 1;
+
+  const handleLogout = async () => {
+    await logout();
+    await goto("/auth");
+  };
 </script>
 
 <div class="max-w-7xl mx-auto flex justify-between items-center p-4">
   <a href="/">
     <img src="/logo-color.svg" alt="Your Analytics logo" width="200" height="32" class="h-8">
   </a>
-  {#if false}
-  <button on:click={logout}>Logout</button>
-  <Nav>
-    <NavItem href="/" label="Home" isActive={$page.path === "/"} />
-    <NavItem href="/privacy" label="Privacy" isActive={$page.path === "/privacy"} />
-    <NavItem href="/docs" label="Docs" isActive={$page.path === "/docs"} />
-  </Nav>
-  <NavMobileMenuButton />
+  {#if $session.user}
+    <div class="flex">
+      <Nav>
+        {#if hasMultipleSites}
+          <NavItem href="/websites" label="Your websites" isActive={$page.path === "/websites"} />
+        {:else}
+          <NavItem href="/{userWebsites[0]}" label="{userWebsites[0]}" isActive={$page.path === `/${userWebsites[0]}`} />
+        {/if}
+      </Nav>
+      <button on:click={handleLogout} class="hidden md:block font-semibold tracking-wide">Sign out</button>
+      <NavMobileMenuButton />
+    </div>
   {/if}
 </div>
 <NavMobile />
--- services/website/src/components/header/nav-mobile.svelte
+++ services/website/src/components/header/nav-mobile.svelte
@@ -1,10 +1,12 @@
 <script>
+  import { goto } from "@sapper/app";
   import { logout } from "../../auth/magic";
   import isOpen from "../../stores/mobile-menu";
   import { session } from "../../stores/session";
 
   $: user = $session.user || {};
-  $: hasMultipleSites = Object.keys(user.sites || {}).length > 1;
+  $: userWebsites = Object.keys(user.sites || {});
+  $: hasMultipleSites = userWebsites.length > 1;
   $: userFirstName = user.firstName || "";
   $: userEmail = user.email || "";
   $: userEmailHash = user.emailHash;
@@ -15,34 +17,31 @@
   };
 </script>
 
-<div class="flex md:hidden">
+<div class="md:hidden">
   {#if $isOpen}
-    <div class="block border-b border-gray-700 md:hidden">
-      {#if hasMultipleSites}
-      <div class="px-2 py-3 sm:px-3">
-        <a href="/websites" class="block px-3 py-2 rounded-md text-base font-medium hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700">Your websites</a>
-        {#if false}
-        <a href="/TODO" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700">Team</a>
-        {/if}
+    {#if false}
+    <div class="pt-2 pb-3">
+      <a href="/EXAMPLE" class="block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out">Your websites</a>
+      <a href="/EXAMPLE" class="mt-1 block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out">Team</a>
+    </div>
+    {/if}
+    <div class="pt-4 pb-3 border-t border-gray-200">
+      <div class="flex items-center px-4">
+        <div class="flex-shrink-0">
+          <img class="h-10 w-10 rounded-full" src="https://www.gravatar.com/avatar/{userEmailHash}.jpg?d=identicon" alt="Profile avatar">
+        </div>
+        <div class="ml-3">
+          <div class="text-base font-medium leading-6 text-gray-800">{userFirstName}</div>
+          <div class="text-sm font-medium leading-5 text-gray-500">{userEmail}</div>
+        </div>
       </div>
-      {/if}
-      <div class="pt-4 pb-3 border-t border-gray-700">
-        <div class="flex items-center px-5">
-          <div class="flex-shrink-0">
-            <img class="h-10 w-10 rounded-full" src="https://www.gravatar.com/avatar/{userEmailHash}.jpg?d=identicon" alt="Profile avatar">
-          </div>
-          <div class="ml-3">
-            <div class="text-base font-medium leading-none">{userFirstName}</div>
-            <div class="mt-1 text-sm font-medium leading-none">{userEmail}</div>
-          </div>
-        </div>
-        <div class="mt-3 px-2" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
-          {#if false}
-          <a href="/TODO" class="block px-3 py-2 rounded-md text-base font-medium hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700" role="menuitem">Your Profile</a>
-          <a href="/TODO" class="mt-1 block px-3 py-2 rounded-md text-base font-medium hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700" role="menuitem">Settings</a>
-          {/if}
-          <button on:click={handleLogout} class="mt-1 block px-3 py-2 rounded-md text-base font-medium hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700" role="menuitem">Sign out</button>
-        </div>
+      <div class="mt-3">
+        {#if hasMultipleSites}
+          <a href="/websites" class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 focus:outline-none focus:text-gray-800 focus:bg-gray-100 transition duration-150 ease-in-out">Your websites</a>
+        {:else}
+          <a href="/{userWebsites[0]}" class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 focus:outline-none focus:text-gray-800 focus:bg-gray-100 transition duration-150 ease-in-out">{userWebsites[0]}</a>
+        {/if}
+        <button on:click={handleLogout} class="mt-1 block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 focus:outline-none focus:text-gray-800 focus:bg-gray-100 transition duration-150 ease-in-out">Sign out</button>
       </div>
     </div>
   {/if}
