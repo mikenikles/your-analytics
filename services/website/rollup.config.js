7037bfa2e238624c22b99e16868802c088b24805
Mike Nikles <mikenikles@gmail.com>
2020-10-03 02:09:38 +0000

    Suppress the THIS_IS_UNDEFINED error message.

--- services/website/rollup.config.js
+++ services/website/rollup.config.js
@@ -18,6 +18,7 @@ const onwarn = (warning, onwarn) =>
   (warning.code === "MISSING_EXPORT" && /'preload'/.test(warning.message)) ||
   (warning.code === "CIRCULAR_DEPENDENCY" &&
     /[/\\]@sapper[/\\]/.test(warning.message)) ||
+  warning.code === "THIS_IS_UNDEFINED" ||
   onwarn(warning);
 
 const sveltePreprocessOptions = sveltePreprocess({
