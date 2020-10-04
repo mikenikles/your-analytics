fdeb5a05ff3e2c537c52ab5f8865aca426e691f0
Mike Nikles <mikenikles@gmail.com>
2020-10-04 15:18:55 +0000

    Add a reusable date range fragment for ClickHouse SQL queries.

---
+++ packages/clickhouse/src/stats/fragments.js
@@ -0,0 +1,2 @@
+export const getDateRange = (dateRange, timezone) =>
+  `toUnixTimestamp(timestamp, '${timezone}') >= toStartOfDay(toDate('${dateRange.from}', '${timezone}'), '${timezone}') AND toUnixTimestamp(timestamp, '${timezone}') < toStartOfDay(addDays(toDate('${dateRange.to}', '${timezone}'), 1), '${timezone}')`;
