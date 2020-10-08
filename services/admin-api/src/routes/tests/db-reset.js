c44205cb3ec87cf6167f912bbd605d1c81b684fa
Mike Nikles <mikenikles@gmail.com>
2020-10-08 23:15:50 +0000

    Add a db-reset endpoint for tests.

---
+++ services/admin-api/src/routes/tests/db-reset.js
@@ -0,0 +1,30 @@
+const { deleteWebsite } = require("@your-analytics/clickhouse");
+const { domainDb, rootDb } = require("@your-analytics/faunadb");
+const express = require("express");
+
+const router = express.Router();
+
+const ignoreError = (promise) => promise.catch((e) => undefined);
+
+router.post("/", async (req, res) => {
+  console.log("[admin-api]: Resetting database...");
+
+  const testUserIssuer = "hello+ya-automated-tests@mikenikles.com";
+  const testUser = await rootDb.users.find(testUserIssuer);
+
+  if (testUser) {
+    const sites = Object.keys(testUser.sites || {});
+    const steps = [];
+    for (const site of sites) {
+      steps.push(domainDb.admin.deleteWebsite(site));
+      steps.push(deleteWebsite(site));
+    }
+    steps.push(rootDb.users.delete(testUserIssuer));
+
+    await Promise.all(steps.map(ignoreError));
+  }
+
+  return res.status(200).end();
+});
+
+module.exports = router;
