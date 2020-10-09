8e39b70e8aabd794fba40a50bd6ba1c1a5816ab0
Mike Nikles <mikenikles@gmail.com>
2020-10-09 23:57:41 +0000

    Convert the ClickHouse fragments.js to CommonJS.

--- packages/clickhouse/src/stats/fragments.js
+++ packages/clickhouse/src/stats/fragments.js
@@ -1,2 +1,6 @@
-export const getDateRange = (dateRange, timezone) =>
+const getDateRange = (dateRange, timezone) =>
   `toUnixTimestamp(timestamp, '${timezone}') >= toStartOfDay(toDate('${dateRange.from}', '${timezone}'), '${timezone}') AND toUnixTimestamp(timestamp, '${timezone}') < toStartOfDay(addDays(toDate('${dateRange.to}', '${timezone}'), 1), '${timezone}')`;
+
+module.exports = {
+  getDateRange,
+};
