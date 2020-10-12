682258167929dafbff96fecddebc02ee1b58931f
Mike Nikles <mikenikles@gmail.com>
2020-10-12 01:00:15 +0000

    Develop the skaffold for the ya-load-testing CLI.

--- services/events-api/ya-load-testing/src/index.ts
+++ services/events-api/ya-load-testing/src/index.ts
@@ -1,28 +1,68 @@
 import { Command, flags } from "@oclif/command";
+import { formatISO, startOfMonth, sub } from "date-fns";
+import got from "got";
 
 class YaLoadTesting extends Command {
   static description = "describe the command here";
 
   static flags = {
-    // add --version flag to show CLI version
     version: flags.version({ char: "v" }),
     help: flags.help({ char: "h" }),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({ char: "n", description: "name to print" }),
-    // flag with no value (-f, --force)
-    force: flags.boolean({ char: "f" }),
+    from: flags.string({
+      char: "f",
+      default: () =>
+        formatISO(
+          startOfMonth(
+            sub(new Date(), {
+              months: 3,
+            })
+          ),
+          { representation: "date" }
+        ),
+      description: "",
+    }),
+    to: flags.string({
+      char: "t",
+      default: () => formatISO(new Date(), { representation: "date" }),
+      description: "",
+    }),
+    min: flags.string({
+      default: "0",
+      description: "",
+    }),
+    max: flags.string({
+      default: "50",
+      description: "",
+    }),
+    period: flags.string({
+      char: "p",
+      default: "hour",
+      description: "",
+      options: ["hour", "day", "month"],
+    }),
   };
 
-  static args = [{ name: "file" }];
-
   async run() {
-    const { args, flags } = this.parse(YaLoadTesting);
+    const { flags } = this.parse(YaLoadTesting);
 
-    const name = flags.name ?? "world";
-    this.log(`hello ${name} from ./src/index.ts`);
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`);
-    }
+    this.log(`Running load testing with flags: %o`, flags);
+
+    await got.post("http://localhost:8080", {
+      body: JSON.stringify({
+        name: "pageview",
+        domain: "local-testing.com",
+        url: "http://local-testing.com/tests",
+        referrer: "http://www.test-referrer.com",
+        screen_size: 800,
+        timestamp: new Date(2020, 5, 28, 12, 13, 14),
+      }),
+      headers: {
+        "Content-Type": "text/plain",
+        "User-Agent":
+          "Mozilla/5.0 (X11; CrOS x86_64 13099.48.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.64 Safari/537.36",
+      },
+    });
+    this.log("Load testing completed.");
   }
 }
 
