ecbc1e5c753181f786a7a9122412330524943c7c
Mike Nikles <mikenikles@gmail.com>
2020-10-16 01:32:32 +0000

    WIP: Use a write buffer to insert multiple events at a time.

--- packages/clickhouse/src/events.js
+++ packages/clickhouse/src/events.js
@@ -1,24 +1,44 @@
 const { convertUrlToDbName } = require("./setup");
 
-const recordEvent = (ch) => (event) =>
-  new Promise((resolve, reject) => {
-    // console.log("Recording event:", JSON.stringify(event));
-    const dbName = convertUrlToDbName(event.domain);
-    const writableStream = ch.query(
-      `INSERT INTO ${dbName}.events`,
-      {
-        format: "JSONEachRow",
-        queryOptions: {
-          input_format_import_nested_json: 1,
-        },
-      },
-      (err) => {
-        err ? reject(err) : resolve();
-      }
-    );
-    writableStream.write(event);
-    writableStream.end();
-  });
+const insertBuffers = {};
+let flushIntervalId;
+
+const recordEvent = (ch) => (event) => {
+  if (!insertBuffers[event.domain]) {
+    insertBuffers[event.domain] = [];
+  }
+  insertBuffers[event.domain].push(event);
+
+  if (!flushIntervalId) {
+    const flushBuffer = () => {
+      Object.entries(insertBuffers).forEach(([domain, events]) => {
+        console.log(`Flushing ${events.length} events for domain ${domain}`);
+        const dbName = convertUrlToDbName(domain);
+        const writableStream = ch.query(
+          `INSERT INTO ${dbName}.events`,
+          {
+            format: "JSONEachRow",
+            queryOptions: {
+              input_format_import_nested_json: 1,
+            },
+          },
+          (err) => {
+            if (err) {
+              console.error(err);
+            }
+          }
+        );
+
+        while (events.length > 0) {
+          writableStream.write(events.shift());
+        }
+        writableStream.end();
+      });
+    };
+    // TODO: Clean this up. Flush also when there are 10k events.
+    flushIntervalId = setInterval(flushBuffer, 5000);
+  }
+};
 
 module.exports = {
   recordEvent,
