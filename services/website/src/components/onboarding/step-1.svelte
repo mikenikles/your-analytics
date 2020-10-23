85126ffadb702cfecca1a37bc231b5a97340de23
Mike Nikles <mikenikles@gmail.com>
2020-10-23 01:13:36 +0000

    Add a next button to step 1; process the enter key as an indication to move to step 2.

--- services/website/src/components/onboarding/step-1.svelte
+++ services/website/src/components/onboarding/step-1.svelte
@@ -1,6 +1,18 @@
 <script lang="ts">
   import onboarding from "../../stores/onboarding";
   import StepWrapper from "./step-wrapper.svelte";
+
+  let firstName = "";
+
+  const handleSubmit = () => {
+    $onboarding.user.firstName = firstName;
+  };
+
+  const handleKeyup = (event) => {
+    if (event.code === "Enter") {
+      handleSubmit();
+    }
+  };
 </script>
 
 <StepWrapper>
@@ -9,7 +21,15 @@
       What's your first name?
     </label>
     <div class="mt-1 rounded-md shadow-sm">
-      <input id="firstName" bind:value={$onboarding.user.firstName} type="text" required class="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5">
+      <input id="firstName" bind:value={firstName} on:keyup|preventDefault={handleKeyup} type="text" required class="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5">
     </div>
+  </div>
+
+  <div class="mt-6">
+    <span class="w-full rounded-md shadow-sm">
+      <button type="button" on:click={handleSubmit} class="w-full flex justify-center mt-4 py-4 px-10 bg-blue-600 rounded-lg font-semibold text-white sm:mt-0">
+        Next: Configure your website
+      </button>
+    </span>
   </div>
 </StepWrapper>
