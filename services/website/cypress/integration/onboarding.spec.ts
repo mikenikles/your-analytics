2cfece2bb284158140db5ea32d8a817039daefef
Mike Nikles <mikenikles@gmail.com>
2020-10-23 01:18:59 +0000

    Update the onboarding test to match the new UX.

--- services/website/cypress/integration/onboarding.spec.ts
+++ services/website/cypress/integration/onboarding.spec.ts
@@ -12,9 +12,10 @@ describe("/onboarding", () => {
   });
 
   it("should create a new website", () => {
-    cy.get('input[name="firstname"]').type("Tester");
-    cy.get('input[name="url"]').type("local-testing.com");
+    cy.get('input[id="firstName"]').type("Tester");
+    cy.get("button[type=button]").click();
+    cy.get('input[id="url"]').type("local-testing.com");
     cy.get('select[name="timezone"]').select("America/Toronto");
-    cy.get("button[type=submit]").click();
+    cy.get("button[type=button]").click();
   });
 });
