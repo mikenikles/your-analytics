f80e56cbf6554fe30c8f7dc77d8a25831751c0ca
Mike Nikles <mikenikles@gmail.com>
2020-10-27 12:18:04 +0000

    Add a Get Started button on the landing page.

--- services/website/src/components/landing-page/hero.svelte
+++ services/website/src/components/landing-page/hero.svelte
@@ -9,10 +9,13 @@
     <div class="py-10 px-4 sm:py-32 sm:px-12">
       <h1 class="text-3xl text-center font-extrabold tracking-wide sm:text-5xl sm:text-left">Your web analytics<br />with privacy and simplicity in mind</h1>
       <p class="mt-8 text-xl sm:text-2xl">Your Analytics - an <strong>open source, privacy-focused</strong> web analytics platform. Focus on the basics and respect visitor privacy.</p>
-      <div class="mt-8 flex justify-center">
-        <a href="/your-analytics.org" class="items-center inline-flex justify-center py-4 px-10 bg-blue-600 rounded-lg font-semibold">
+      <div class="mt-8 justify-center sm:flex">
+        <a href="/onboarding" class="w-full items-center inline-flex justify-center py-4 px-10 bg-blue-600 rounded-lg font-semibold sm:w-auto">
+          Get Started
+        </a>
+        <a href="/your-analytics.org" class="mt-4 w-full items-center inline-flex justify-center py-4 px-10 bg-white rounded-lg font-semibold text-black sm:mt-0 sm:ml-4 sm:w-auto">
           <span class="mr-2 relative flex h-3 w-3">
-            <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-white opacity-75"></span>
+            <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-black opacity-75"></span>
             <span class="relative inline-flex rounded-full h-3 w-3 bg-pink-500"></span>
           </span>
           Live Demo
