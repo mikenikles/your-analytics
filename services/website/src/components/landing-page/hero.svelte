7dc2a6edbf116ab4cf5e972530da1f2a58aae55a
Mike Nikles <mikenikles@gmail.com>
2020-10-27 12:05:25 +0000

    Remove the beta signup form & backend code.

--- services/website/src/components/landing-page/hero.svelte
+++ services/website/src/components/landing-page/hero.svelte
@@ -1,16 +1,3 @@
-<script lang="ts">
-  import { recordEmail } from "../../api/beta";
-
-  let isEmailAdded: boolean;
-  let betaSignUpForm: HTMLFormElement;
-
-  const handleSubmit = async () => {
-    const formData = new FormData(betaSignUpForm);
-    const email = formData.get("email");
-    isEmailAdded = await recordEmail(email);
-  };
-</script>
-
 <style>
   .bg {
     background-image: url('/logo-3d.png');
@@ -30,27 +17,6 @@
           </span>
           Live Demo
         </a>
-      </div>
-    </div>
-    <div class="pb-32 px-4 sm:px-20">
-      <div class="shadow-2xl">
-        <div class="p-4 bg-pink-500 text-xl font-bold sm:text-2xl">Follow us for updates and early access</div>
-        <form bind:this={betaSignUpForm} on:submit|preventDefault={handleSubmit} class="bg-white">
-          <div class="p-4 flex flex-col sm:flex-row">
-            <div class="mt-1 flex flex-1 rounded-md shadow-sm">
-              <span class="inline-flex items-center px-4 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 sm:text-sm">
-                @
-              </span>
-              <input placeholder="Enter your email" aria-label="Email" name="email" class="form-input flex-1 px-4 py-2 rounded-none rounded-r-md text-black sm:text-sm sm:leading-5">
-            </div>
-            <button type="submit" class="mt-4 py-4 px-10 bg-blue-600 rounded-lg font-semibold sm:ml-4 sm:mt-0">Notify me!</button>
-          </div>
-          {#if isEmailAdded}
-            <p class="p-4 text-center text-normal font-bold text-black">
-              Fantastic, we will be in touch.
-            </p>
-          {/if}
-        </form>
       </div>
     </div>
   </div>
