3a89648421aa55201db0a8a9544f9e234633d475
Mike Nikles <mikenikles@gmail.com>
2020-10-27 12:43:11 +0000

    Re-enable analytics on the index page.

--- services/website/src/routes/index.svelte
+++ services/website/src/routes/index.svelte
@@ -44,7 +44,7 @@
 
 <svelte:head>
 	<title>Web analytics platform. Open source, privacy-focused and simple. | Your Analytics</title>
-  <!-- {@html '<script async src="https://your-analytics.org/ya.js" data-domain="your-analytics.org"></script>'} -->
+  {@html '<script async src="https://your-analytics.org/ya.js" data-domain="your-analytics.org"></script>'}
 </svelte:head>
 
 <Header />
