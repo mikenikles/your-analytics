4fe6810f88727953cb052805288d8ba8d8afbd42
Mike Nikles <mikenikles@gmail.com>
2020-11-07 00:30:04 +0000

    Import the @testing-library/cypress commands.

--- services/website/cypress/support/commands.ts
+++ services/website/cypress/support/commands.ts
@@ -24,6 +24,8 @@
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
+import "@testing-library/cypress/add-commands";
+
 Cypress.Commands.add("loginWithApi", () => {
   return cy.request(
     "POST",
