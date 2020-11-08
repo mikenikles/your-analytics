dcb09f9f956beae80f6424ec2b57f494d5640b17
Mike Nikles <mikenikles@gmail.com>
2020-11-08 00:12:20 +0000

    Remove debug console statement.

--- services/website/cypress/plugins/db.ts
+++ services/website/cypress/plugins/db.ts
@@ -2,35 +2,10 @@ import fetch from "node-fetch";
 
 const tasks = {
   async "db:reset"() {
-    console.log("FETCH", fetch);
     await fetch("http://localhost:3000/api/admin/tests/db-reset", {
       method: "POST",
     });
     return {};
-    // return new Promise((resolve, reject) => {
-    //   const callback = (res) => {
-    //     res.on("error", () => {
-    //       reject(null);
-    //     });
-
-    //     res.on("data", () => {});
-
-    //     res.on("end", () => {
-    //       resolve(null);
-    //     });
-    //   };
-    //   http
-    //     .request(
-    //       {
-    //         host: "localhost",
-    //         method: "POST",
-    //         path: "/api/admin/tests/db-reset",
-    //         port: 3000,
-    //       },
-    //       callback
-    //     )
-    //     .end();
-    // });
   },
 };
 
