b6a8fdc6886eff881468960606bbb06aacfcdef6
Mike Nikles <mikenikles@gmail.com>
2020-11-08 00:10:22 +0000

    Use node-fetch to simplify the db:reset code.

--- services/website/cypress/plugins/db.ts
+++ services/website/cypress/plugins/db.ts
@@ -1,31 +1,36 @@
-import * as http from "http";
+import fetch from "node-fetch";
 
 const tasks = {
   async "db:reset"() {
-    return new Promise((resolve, reject) => {
-      const callback = (res) => {
-        res.on("error", () => {
-          reject(null);
-        });
-
-        res.on("data", () => {});
-
-        res.on("end", () => {
-          resolve(null);
-        });
-      };
-      http
-        .request(
-          {
-            host: "localhost",
-            method: "POST",
-            path: "/api/admin/tests/db-reset",
-            port: 3000,
-          },
-          callback
-        )
-        .end();
+    console.log("FETCH", fetch);
+    await fetch("http://localhost:3000/api/admin/tests/db-reset", {
+      method: "POST",
     });
+    return {};
+    // return new Promise((resolve, reject) => {
+    //   const callback = (res) => {
+    //     res.on("error", () => {
+    //       reject(null);
+    //     });
+
+    //     res.on("data", () => {});
+
+    //     res.on("end", () => {
+    //       resolve(null);
+    //     });
+    //   };
+    //   http
+    //     .request(
+    //       {
+    //         host: "localhost",
+    //         method: "POST",
+    //         path: "/api/admin/tests/db-reset",
+    //         port: 3000,
+    //       },
+    //       callback
+    //     )
+    //     .end();
+    // });
   },
 };
 
