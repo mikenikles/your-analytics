804d09da6fcd048e97352514ddcc81a9efee7423
Mike Nikles <mikenikles@gmail.com>
2020-11-09 16:26:29 +0000

    WIP: Figure out why tests on GitHub fail.

--- packages/clickhouse/src/index.js
+++ packages/clickhouse/src/index.js
@@ -4,6 +4,7 @@ const { addNewWebsite, convertUrlToDbName } = require("./setup");
 const stats = require("./stats");
 const { deleteWebsite } = require("./tests");
 
+console.log("TEMP: CH_HOST: %s", process.env.CH_HOST);
 const ch = new ClickHouse({
   host: process.env.CH_HOST,
   port: process.env.CH_PORT,
