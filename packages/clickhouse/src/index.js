397c2f4d6d0a1c089e03577cb3d5bfbd1de6af79
Mike Nikles <mikenikles@gmail.com>
2020-11-09 16:34:03 +0000

    WIP: Use the default localhost to connect to the ClickHouse service.

--- packages/clickhouse/src/index.js
+++ packages/clickhouse/src/index.js
@@ -4,7 +4,6 @@ const { addNewWebsite, convertUrlToDbName } = require("./setup");
 const stats = require("./stats");
 const { deleteWebsite } = require("./tests");
 
-console.log("TEMP: CH_HOST: %s", process.env.CH_HOST);
 const ch = new ClickHouse({
   host: process.env.CH_HOST,
   port: process.env.CH_PORT,
