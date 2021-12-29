c6c5731cf9411d554ed26ef838a88d2d841dca29
Mike Nikles <mikenikles@pm.me>
2021-12-29 20:53:34 +0000

    Use the dev-test.com domain for testing.

--- services/website/cypress/integration/onboarding.spec.ts
+++ services/website/cypress/integration/onboarding.spec.ts
@@ -12,7 +12,7 @@ describe("/onboarding", () => {
   });
 
   it("should create a new website", () => {
-    const websiteUrl = "dev.com";
+    const websiteUrl = "dev-test.com";
     cy.findByLabelText("What's your first name?").type("Tester");
     cy.findByText("Next: Configure your website").click();
     cy.findByLabelText("What's your website URL?").type(websiteUrl);
